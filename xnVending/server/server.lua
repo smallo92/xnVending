7be106515363c33c3266f1381f6b67dc6c6e5f01
Vegard Bendiktsen <glitchdetector@gmail.com>
2020-03-21 19:12:10 +0100

    Refactored code to allow framework differences
    Server side is hit very hard, it's more or less separated by framework now.
    Added Standalone option, it just ignores everything and says "ok" to vend items.
    Added the vRP logic.
    Non-ESX frameworks use a somewhat copy of the ESX callback logic. (good system!)
    Items are now given when the animation ends, rather than when the animation starts. Same with money transaction, even if it should probably happen upon vend. (Checks are still in place to deny you a vend initially!)
    
    Needs a double check to make sure the ESX verison still works, since I can't test it.

--- xnVending/server/server.lua
+++ xnVending/server/server.lua
@@ -1,42 +1,96 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+if Config.Framework == "ESX" or Config.Framework == "NewESX" then
+	ESX = nil
+	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('esx_vending:checkMoneyandInvent', function(source, cb, item, count)
-	local plySource = source
-	local thisItem = item
-	local thisCount = count
-	local thisCost = thisItem.price[thisCount]
-	local xPlayer = ESX.GetPlayerFromId(plySource)
-	local targetItem = xPlayer.getInventoryItem(thisItem.item[thisCount])
-	
-	if Config.NewEsx then
-		local cashMoney = xPlayer.getAccount('money').money
-		if thisCost > cashMoney then
-			cb("cash")
-		else
-			if not xPlayer.canCarryItem(thisItem.item[thisCount], 1) then
-				cb("inventory")
+	function AddItem(thisCost, thisItem, xPlayer)
+		xPlayer.removeMoney(thisCost)
+		xPlayer.addInventoryItem(thisItem, 1)
+	end
+
+	-- confirm parameter is used for after the animation plays, do the same checks but also give item and take money
+	ESX.RegisterServerCallback('esx_vending:checkMoneyandInvent', function(source, cb, machine, entry, confirm)
+		local thisItem = Config.Machines[machine]
+		local thisCost = thisItem.price[entry]
+		local xPlayer = ESX.GetPlayerFromId(source)
+		local targetItem = xPlayer.getInventoryItem(thisItem.item[entry])
+
+		if Config.Framework == "NewESX" then
+			local cashMoney = xPlayer.getAccount('money').money
+			if thisCost > cashMoney then
+				cb("cash")
 			else
-				AddItem(thisCost, thisItem.item[thisCount], xPlayer)
-				cb(true)
+				if not xPlayer.canCarryItem(thisItem.item[entry], 1) then
+					cb("inventory")
+				else
+					if confirm then AddItem(thisCost, thisItem.item[entry], xPlayer) end
+					cb(true)
+				end
+			end
+		else
+			local cashMoney = xPlayer.getMoney()
+			if thisCost > cashMoney then
+				cb("cash")
+			else
+				if targetItem.limit ~= -1 and (targetItem.count + 1) > targetItem.limit then
+					cb("inventory")
+				else
+					if confirm then AddItem(thisCost, thisItem.item[entry], xPlayer) end
+					cb(true)
+				end
 			end
 		end
-	else
-		local cashMoney = xPlayer.getMoney()
-		if thisCost > cashMoney then
-			cb("cash")
-		else
-			if targetItem.limit ~= -1 and (targetItem.count + 1) > targetItem.limit then
-				cb("inventory")
-			else
-				AddItem(thisCost, thisItem.item[thisCount], xPlayer)
+	end)
+elseif Config.Framework == "vRP" then
+	-- vRP Compatibility
+	local Proxy = module("vrp", "lib/Proxy")
+	vRP = Proxy.getInterface("vRP")
+	TryGiveInventoryItem = function(source, cb, machine, entry, confirm)
+		local user_id = vRP.getUserId({source})
+		local item = Config.Machines[machine]
+		local itemid = item.item[entry]
+		local price = item.price[entry]
+		-- Make sure we can afford the item
+		if vRP.getMoney({user_id}) >= price then
+			local weight = vRP.getItemWeight({itemid})
+			local remaining = vRP.getInventoryMaxWeight({user_id}) - vRP.getInventoryWeight({user_id})
+			-- Make sure we can carry the item
+			if weight <= remaining then
+				-- Give and pay
+				if confirm then
+					vRP.giveInventoryItem({user_id, itemid, 1, true})
+					vRP.tryPayment({user_id, price})
+				end
 				cb(true)
+			else
+				cb("inventory")
 			end
+		else
+			cb("cash")
 		end
 	end
-end)
 
-function AddItem(thisCost, thisItem, xPlayer)
-	xPlayer.removeMoney(thisCost)
-	xPlayer.addInventoryItem(thisItem, 1)
+	RegisterServerEvent('xnVending:triggerServerCallback')
+	AddEventHandler('xnVending:triggerServerCallback', function(name, requestId, ...)
+		local playerId = source
+		if name == "esx_vending:checkMoneyandInvent" then
+			TryGiveInventoryItem(playerId, function(...)
+				TriggerClientEvent('xnVending:serverCallback', playerId, requestId, ...)
+			end, ...)
+		end
+	end)
+else
+	-- Standalone, no economy so we just instantly accept it
+	TryGiveInventoryItem = function(source, cb, machine, entry)
+		cb(true)
+	end
+
+	RegisterServerEvent('xnVending:triggerServerCallback')
+	AddEventHandler('xnVending:triggerServerCallback', function(name, requestId, ...)
+		local playerId = source
+		if name == "esx_vending:checkMoneyandInvent" then
+			TryGiveInventoryItem(playerId, function(...)
+				TriggerClientEvent('xnVending:serverCallback', playerId, requestId, ...)
+			end, ...)
+		end
+	end)
 end
